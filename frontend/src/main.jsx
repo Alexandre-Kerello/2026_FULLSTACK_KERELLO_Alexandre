42422ad1008100cc05a9fdf52d73ba0e135f6e41
Alexandre KERELLO <akerello@gmail.com>
2026-03-20 15:54:53 +0100

    add Home + Error pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Home from './pages/public/Home.jsx'
+import Login from './pages/public/Login.jsx'
+import ErrorPage from './pages/public/Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
 )
