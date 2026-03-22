af469bfbddaad005f0d6c2cc5b74c49f8dc5dc10
Alexandre KERELLO <akerello@gmail.com>
2026-03-22 22:05:31 +0100

    add error 404 page

--- frontend/src/pages/public/Error.jsx
+++ frontend/src/pages/public/Error.jsx
@@ -5,12 +5,29 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            Page introuvable
+          </h1>
+          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            Désolé, nous n'avons pas pu trouver la page que vous recherchez.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="/"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Revenir à l'accueil <span aria-hidden="true">&rarr;</span>
+            </a>
+            <a href="/contact" className="rounded px-3.5 py-2.5 text-sm font-semibold text-gray-900 hover:outline-2 hover:outline-gray-400">
+              Obtenir de l'aide <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
